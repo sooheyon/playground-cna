16c27144af8fabecd170e3d978285880a0f1d8af
sooheyon <sooheyonjo@gmail.com>
2024-02-21 11:50:30 +0900

    prisma init

--- app/a/page.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-
-const A: NextPage = () => {
-  return <div>A</div>;
-};
-
-export default A;
--- app/b/page.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-
-const B: NextPage = () => {
-  return <div>B</div>;
-};
-
-export default B;
